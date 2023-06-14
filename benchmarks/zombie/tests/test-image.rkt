2f871edd55089f062784d3512527cc1290a538b7
amyzliao <amzhliao@gmail.com>
2023-06-14 12:11:18 -0500

    image empty scene tests

--- benchmarks/zombie/tests/test-image.rkt
+++ benchmarks/zombie/tests/test-image.rkt
@@ -1,20 +1,73 @@
 #lang racket
-
 (require rackunit
          rackunit/text-ui
          "../untyped/image.rkt")
 
-(define test-suite-name
+;;;;????? what
+; (struct-out image)
+#|
+(struct image (
+ impl
+))
+|#
+
+
+#|
+(define (empty-scene w h)
+  (when (or (negative? w) (negative? h))
+    (error 'image "Arguments must be non-negative real numbers"))
+  (image (cons w h)))
+|#
+(define empty-scene-tests
   (test-suite
-   "name of test suite"
-   #:before (lambda () (display "START\n"))
-   #:after  (lambda () (display "FINISH\n"))
-   (check-eq? 1 1)
-   (check-eq? 2 2)
+   "empty-scene-tests"
+   #:before (lambda () (display "START empty-scene-tests\n"))
+   #:after  (lambda () (display "FINISH empty-scene-tests\n"))
    (test-case
-    "name of test case"
-    (check < 1 2)
-    (check > 3 2))
+    "not enough args"
+    ;(check-exn-h (empty-scene))
+    (check-exn exn:fail? (lambda () (empty-scene)))
+    (check-exn exn:fail? (lambda () (empty-scene 1 2 23 5 6)))
+    (check-exn exn:fail? (lambda () (empty-scene 10)))
+    )
+   (test-case
+    "invalid w h"
+    (check-exn exn:fail? (lambda () (empty-scene 'dog "cat")))
+    (check-exn exn:fail? (lambda () (empty-scene "dog" 'cat)))
+    (check-exn exn:fail? (lambda () (empty-scene 'dog 'cat)))
+    (check-exn exn:fail? (lambda () (empty-scene 1 "poggers")))
+    (check-exn exn:fail? (lambda () (empty-scene "poggers" 1)))
+    (check-exn exn:fail? (lambda () (empty-scene 1 -1)))
+    (check-exn exn:fail? (lambda () (empty-scene -1 -1)))
+    (check-exn exn:fail? (lambda () (empty-scene -1 1)))
+    )
+   (test-suite
+    "correct results"
+    (test-case
+     "(empty-scene 1 3)"
+     (check-true (image? (empty-scene 1 3)))
+     (check-true (cons? (image-impl (empty-scene 1 3))))
+     (check-equal? 1 (car (image-impl (empty-scene 1 3))))
+     (check-equal? 3 (cdr (image-impl (empty-scene 1 3))))
+    )
+    (test-case
+     "(empty-scene 1111 999)"
+     (check-true (image? (empty-scene 1111 999)))
+     (check-true (cons? (image-impl (empty-scene 1111 999))))
+     (check-equal? 1111 (car (image-impl (empty-scene 1111 999))))
+     (check-equal? 999 (cdr (image-impl (empty-scene 1111 999))))
+    ))
    ))
 
-(run-tests test-suite-name)
+#|
+(define (place-image i1 w h i2)
+  (image (list i1 w h i2)))
+|#
+
+#|
+(define (circle radius style color)
+  (image (list radius style color)))
+|# 
+
+
+(run-tests empty-scene-tests)
