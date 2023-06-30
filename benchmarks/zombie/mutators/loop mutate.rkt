2341bca0c619a366419c9fb2c3da1e6437a0e254
HZZX0318 <zexuanzhou2025@u.northwestern.edu>
2023-06-30 14:06:49 -0500

    added display and comment

--- benchmarks/zombie/mutators/loop mutate.rkt
+++ benchmarks/zombie/mutators/loop mutate.rkt
@@ -54,19 +54,28 @@
 (define second-mutant (second mutants))
 
 ;loop through mutants
+;variable storing current mutant working on
 (define cur-mutant #f)
+;the file that stores mutant
 (define cur-out #f)
+;variable storing total number of mutants successfully identifed
 (define test-fail 0)
 (for ([i x])
-  (display (string-append "Running Mutant" (number->string i) "\n"))
+  (display (string-append "Running Mutant" (number->string (+ i 1)) "\n"))
+  ;get current mutant to work on
   (set! cur-mutant (list-ref mutants i))
   (set! cur-out (open-output-file "curmutant.rkt"))
   (pretty-write cur-mutant cur-out)
   (close-output-port cur-out)
+  ;run test on mutant, increment test-fail if test gives error or fail.
   (if (parameterize ([current-output-port (open-output-nowhere)]
                      [current-error-port (open-output-nowhere)]) (system* (whereis-system 'exec-file) (whereis-raco "test") "tests/test-math.rkt"))
-      (display (string-append "Mutant " (number->string i) " not identified, " (number->string test-fail) " in total\n"))
+      (display (string-append "Mutant " (number->string (+ i 1)) " not identified, " (number->string test-fail) " in total\n"))
       (begin (set! test-fail (+ test-fail 1))
-             (display (string-append "Mutant " (number->string i) " identified, " (number->string test-fail) " in total\n"))))
+             (display (string-append "Mutant " (number->string (+ i 1)) " identified, " (number->string test-fail) " in total\n"))))
+  ;delete and clean for next round
   (delete-file "curmutant.rkt"))
 
+(define res (exact->inexact (/ test-fail x)))
+
+(display (string-append (number->string test-fail) " mutants identified out of " (number->string x) ", mutation score of " (number->string res)))
